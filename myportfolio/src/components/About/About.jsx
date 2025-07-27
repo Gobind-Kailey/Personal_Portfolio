7b563c74e4c6a1acc522493e9097f7eb9d5b572d
kaileg2 <gobind2975@gmail.com>
2025-07-27 14:59:26 -0400

    Adding all required files

---
+++ myportfolio/src/components/About/About.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactTypingEffect from 'react-typing-effect';
+
+const About = () => {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+
+export default About;
