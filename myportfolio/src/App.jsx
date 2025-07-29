2b1cd0e85f708bf857acc9adf22c16a610d09124
kaileg2 <gobind2975@gmail.com>
2025-07-29 10:09:01 -0400

    Finished Creating NavBar

--- myportfolio/src/App.jsx
+++ myportfolio/src/App.jsx
@@ -21,7 +21,7 @@ const App = () => {
       <div className="absolute inset-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]"></div>
     
       {/* Main Content */}
-      <div className="relativept-20">
+      <div className="relative pt-20">
         <Navbar />
         <About />
         <Skills />
@@ -30,9 +30,14 @@ const App = () => {
         <Education />
         <Contact />
         <Footer />
+
+        {/* Optional scroll test sections (Debugging purposes)
+        <section id="scroll-test" className="h-[100vh] bg-red-500 text-white text-center pt-20">
+          Scroll Test Section
+        </section> */}
       </div>
 
-    </div>
+    </div>     
   );
 };
 
