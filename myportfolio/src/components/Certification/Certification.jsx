ba8ebe489126d664d78f1bf19d120d5205887d8e
kaileg2 <gobind2975@gmail.com>
2025-09-20 16:24:45 -0400

    Updating size of Certificates image

--- myportfolio/src/components/Certification/Certification.jsx
+++ myportfolio/src/components/Certification/Certification.jsx
@@ -1,12 +1,11 @@
-import React, {useState} from 'react'
-import { certifications } from "../../constants"; 
-
+import React, { useState } from "react";
+import { certifications } from "../../constants";
 
 const Certificate = () => {
   const [selectedProject, setSelectedProject] = useState(null);
 
-  const handleOpenModal = (certifications) => {
-    setSelectedProject(certifications);
+  const handleOpenModal = (cert) => {
+    setSelectedProject(cert);
   };
 
   const handleCloseModal = () => {
@@ -27,30 +26,30 @@ const Certificate = () => {
         </p>
       </div>
 
-      {/* Projects Grid */}
+      {/* Certifications Grid */}
       <div className="grid gap-12 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-        {certifications.map((certifications) => (
+        {certifications.map((cert) => (
           <div
-            key={certifications.id}
-            onClick={() => handleOpenModal(certifications)}
+            key={cert.id}
+            onClick={() => handleOpenModal(cert)}
             className="border border-white bg-gray-900 backdrop-blur-md rounded-2xl shadow-2xl overflow-hidden cursor-pointer hover:shadow-purple-500/50 hover:-translate-y-2 transition-transform duration-300"
           >
             <div className="p-4">
               <img
-                src={certifications.image}
-                alt={certifications.title}
+                src={cert.image}
+                alt={cert.title}
                 className="w-full h-48 object-contain rounded-xl"
               />
             </div>
             <div className="p-6">
               <h3 className="text-2xl font-bold text-white mb-2">
-                {certifications.title}
+                {cert.title}
               </h3>
               <p className="text-gray-500 mb-4 pt-4 line-clamp-3">
-                {certifications.description}
+                {cert.description}
               </p>
               <div className="mb-4">
-                {certifications.tags.map((tag, index) => (
+                {cert.tags.map((tag, index) => (
                   <span
                     key={index}
                     className="inline-block bg-[#251f38] text-xs font-semibold text-purple-500 rounded-full px-2 py-1 mr-2 mb-2"
@@ -64,10 +63,10 @@ const Certificate = () => {
         ))}
       </div>
 
-      {/* Modal Container */}
+      {/* Modal */}
       {selectedProject && (
         <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-90 p-4">
-          <div className="bg-gray-900 rounded-xl shadow-2xl lg:w-full w-[90%] max-w-3xl overflow-hidden relative">
+          <div className="bg-gray-900 rounded-xl shadow-2xl lg:w-full w-[90%] max-w-3xl overflow-y-auto max-h-[90vh] relative">
             <div className="flex justify-end p-4">
               <button
                 onClick={handleCloseModal}
@@ -82,7 +81,7 @@ const Certificate = () => {
                 <img
                   src={selectedProject.image}
                   alt={selectedProject.title}
-                  className="max-w-full max-h-[500px] object-contain mx-auto rounded-xl shadow-2xl"
+                  className="max-w-full max-h-[70vh] object-contain mx-auto rounded-xl shadow-2xl"
                 />
               </div>
               <div className="lg:p-8 p-6">
