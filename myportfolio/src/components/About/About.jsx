f74a48ef25ee3f9aa2a195cd32f4c70e15b67e00
kaileg2 <gobind2975@gmail.com>
2025-10-31 00:38:00 -0400

    Updating Resume

--- myportfolio/src/components/About/About.jsx
+++ myportfolio/src/components/About/About.jsx
@@ -51,18 +51,7 @@ I'm currently building my skills through hands-on side projects involving FPGA p
 Outside of academics, I love going to the gym and climbing. I also enjoy connecting with people through shared interests and meaningful collaboration.
           </p>
           {/* Resume Button */}
-          <a
-            href="https://drive.google.com/file/d/1aWxy7QkeqRwYLVHpRpXY-uxH__I4Ff9Z/view?usp=sharing"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
-            style={{
-              background: 'linear-gradient(90deg, #8245ec, #a855f7)',
-              boxShadow: '0 0 2px #8245ec, 0 0 2px #8245ec, 0 0 40px #8245ec',
-            }}
-          >
-            DOWNLOAD CV
-          </a>
+          
         </div>
 
         {/* Right Side */}
